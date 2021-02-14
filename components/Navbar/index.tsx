5b9771691069b2c70c894df435dabeedbd0b48ac
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-02-14 19:09:01 -0300

    Fix code style

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+
 import { Nav, Navbar as NavbarComponent } from 'react-bootstrap'
 import { resolveLanguage } from '../../utils'
 import translations from './translations'
