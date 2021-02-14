0aabb183a16e2d1eb01d826de8fcbf46e727e446
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-02-14 18:25:21 -0300

    Install bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from 'next/app'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
