5ff9301ad85b10d26587f9fa21d54bd32b13897c
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-02-18 23:19:23 -0300

    Refactore faq page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-
+import { Col, Container, Row } from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
+
+import Navbar from '../components/Navbar'
 import { getFAQ } from '../lib/faq'
+import { resolveLanguage } from '../utils'
 
 interface FaqPageProps {
   faq: string
@@ -11,19 +14,35 @@ interface FaqPageProps {
 
 const FaqPage: NextPage<FaqPageProps> = ({ faq }) => {
   const router = useRouter()
+  const locale = resolveLanguage(router.locale)
+  const translation = translations[locale]
 
   return (
     <>
       <Head>
-        <title>Titulo</title>
+        <title>{translation.title}</title>
       </Head>
-      <section>
-        <ReactMarkdown>
-          {faq}
-        </ReactMarkdown>
-      </section>
+      <Navbar />
+      <Container style={{ marginTop: 55 }} className='pt-3'>
+        <Row>
+          <Col sm='12'>
+            <ReactMarkdown>
+              {faq}
+            </ReactMarkdown>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
+}
+
+const translations = {
+  'en-US': {
+    title: 'FAQ - NIZKCTF',
+  },
+  'pt-BR': {
+    title: 'FAQ - NIZKCTF',
+  },
 }
 
 export const getStaticProps: GetStaticProps = async ({ }) => {
