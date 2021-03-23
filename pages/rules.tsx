4fc43dec9d03a1771ef5eb94567799411b1cb6ad
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-03-22 23:49:48 -0300

    Change rule page style

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -2,9 +2,12 @@ import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
+import { Col, Container, Row } from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
 
 import { getRules } from '../lib/rules'
+import { resolveLanguage } from '../utils'
+import Navbar from '../components/Navbar'
 
 interface RulePageProps {
   rules: string
@@ -12,19 +15,35 @@ interface RulePageProps {
 
 const RulePage: NextPage<RulePageProps> = ({ rules }) => {
   const router = useRouter()
+  const locale = resolveLanguage(router.locale)
+  const translation = translations[locale]
 
   return (
     <>
-      <Head>
-        <title>Titulo</title>
-      </Head>
-      <section>
-        <ReactMarkdown>
-          {rules}
-        </ReactMarkdown>
-      </section>
-    </>
+    <Head>
+      <title>{translation.title}</title>
+    </Head>
+    <Navbar />
+    <Container style={{ marginTop: 55 }} className='pt-3'>
+      <Row>
+        <Col sm='12'>
+          <ReactMarkdown>
+            {rules}
+          </ReactMarkdown>
+        </Col>
+      </Row>
+    </Container>
+  </>
   )
+}
+
+const translations = {
+  'en-US': {
+    title: 'Rules - NIZKCTF',
+  },
+  'pt-BR': {
+    title: 'Regras - NIZKCTF',
+  },
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
