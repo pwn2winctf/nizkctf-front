bb022ca41665758a52f8748ab5e712560ff9efe7
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-04-09 12:37:34 -0300

    Update CMS url

--- lib/rules.ts
+++ lib/rules.ts
@@ -1,7 +1,7 @@
 const RULE_BASE_URL = 'https://pwn2.win'
 
 export const getRules = async (language: string) => {
-  const path = language === 'pt-BR' ? '/NIZKCTF-js/regras.md' : '/NIZKCTF-js/rules.md'
+  const path = language === 'pt-BR' ? '/nizkctf-content/regras.md' : '/nizkctf-content/rules.md'
   const url = new URL(path, RULE_BASE_URL).toString()
 
   const rules: string = await fetch(url)
