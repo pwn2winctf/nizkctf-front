38e4bba6e39029f19b9ed02ec0b1935876ae628b
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-04-15 01:35:37 -0300

    Refactore to request share info

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -17,7 +17,19 @@ const SignUpPage: NextPage = () => {
 
   const { user } = useAuth()
 
-  const [values, setValues] = useState<{ email: string, password: string, name: string }>({ email: '', password: '', name: '' })
+  const [values, setValues] = useState<{
+    email: string,
+    password: string,
+    confirmPassword: string,
+    name: string,
+    shareInfo: boolean
+  }>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    name: '',
+    shareInfo: false
+  })
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -31,8 +43,20 @@ const SignUpPage: NextPage = () => {
       return
     }
 
-
     try {
+      const { isConfirmed } = await swal.fire({
+        title: translation.modal.infoTitle,
+        icon: 'info',
+        showCancelButton: true,
+        cancelButtonText: translation.cancel,
+        showConfirmButton: true,
+        confirmButtonText: translation.confirm,
+      })
+
+      if (!isConfirmed) {
+        return
+      }
+
       await signUp(values)
 
       swal.fire(
@@ -48,6 +72,9 @@ const SignUpPage: NextPage = () => {
       )
     }
   }
+
+  const isFilled = values.name.length > 0 && values.email.length > 0 && values.password.length > 0 && values.password === values.confirmPassword
+
   return (
     <>
       <Head>
@@ -62,21 +89,57 @@ const SignUpPage: NextPage = () => {
                 <Form.Label>
                   {translation.name}
                 </Form.Label>
-                <Form.Control type='text' name='name' value={values.name} onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
+                <Form.Control
+                  type='text'
+                  name='name'
+                  value={values.name}
+                  required
+                  onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
               </Form.Group>
               <Form.Group>
                 <Form.Label>
                   {translation.email}
                 </Form.Label>
-                <Form.Control type='text' name='email' value={values.email} onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
+                <Form.Control
+                  type='email'
+                  name='email'
+                  value={values.email}
+                  required
+                  onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
               </Form.Group>
               <Form.Group>
                 <Form.Label>
                   {translation.password}
                 </Form.Label>
-                <Form.Control type='password' name='password' value={values.password} onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
+                <Form.Control
+                  type='password'
+                  name='password'
+                  value={values.password}
+                  required
+                  onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
               </Form.Group>
-              <Button variant='primary' type='submit'>
+              <Form.Group>
+                <Form.Label>
+                  {translation.confirmPassword}
+                </Form.Label>
+                <Form.Control
+                  type='password'
+                  name='confirmPassword'
+                  value={values.confirmPassword}
+                  required
+                  {...values.confirmPassword !== values.password && { style: { borderColor: 'red' } }}
+                  onChange={event => setValues({ ...values, [event.target.name]: event.target.value })} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Check
+                  type='checkbox'
+                  name='shareInfo'
+                  label={translation.shareInfo}
+                  value='shareInfo'
+                  checked={values.shareInfo}
+                  onChange={event => setValues({ ...values, shareInfo: !values.shareInfo })} />
+              </Form.Group>
+              <Button variant='primary' type='submit' disabled={!isFilled}>
                 {translation.signUp}
               </Button>
             </Form>
@@ -94,24 +157,34 @@ const translations = {
     signUp: 'Register',
     email: 'E-mail',
     password: 'Password',
+    confirmPassword: 'Confirm password',
     name: 'Name',
+    shareInfo: 'I am interested in receiving possible job offers from sponsors',
     modal: {
+      infoTitle: 'This user will be used by your team to submit flags',
       successfullyTitle: 'Check your email to complete your registration!',
       warningTitle: 'You are already logged in!',
       errorTitle: 'Error!',
     },
+    confirm: 'Confirm',
+    cancel: 'Cancel',
   },
   'pt-BR': {
     title: 'Registrar-se - NIZKCTF',
     signUp: 'Registrar-se',
     email: 'E-mail',
     password: 'Senha',
+    confirmPassword: 'Confirmar senha',
     name: 'Nome',
+    shareInfo: 'Tenho interesse em receber eventuais propostas de\n emprego dos patrocinadores',
     modal: {
+      infoTitle: 'Esse usuário será usado pelo seu time para enviar flags',
       successfullyTitle: 'Verifique seu e-mail para completar seu cadastro!',
       warningTitle: 'Você já está logado!',
       errorTitle: 'Ops, aconteceu um erro!',
     },
+    confirm: 'Confirmar',
+    cancel: 'Cancelar',
   },
 }
 
