801f6c194662ac19d445569fe3ba1a159b61ed6a
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-04-15 00:30:30 -0300

    Update FAQ

--- lib/faq.ts
+++ lib/faq.ts
@@ -1,7 +1,7 @@
 const FAQ_BASE_URL = 'https://pwn2winctf.github.io'
 
 export const getFAQ = async () => {
-  const url = new URL('/NIZKCTF-js/FAQ.md', FAQ_BASE_URL).toString()
+  const url = new URL('/nizkctf-content/FAQ.md', FAQ_BASE_URL).toString()
 
   const faq: string = await fetch(url)
     .then(response => response.text())
