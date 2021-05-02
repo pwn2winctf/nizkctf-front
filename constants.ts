08f9c2f80a39765ebbdf0853ab9f9f2697f92b79
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-05-02 11:45:25 -0300

    Update challenges endpoint

--- constants.ts
+++ constants.ts
@@ -14,7 +14,7 @@ export const NAME = name
 export const VERSION = version
 
 export const API_BASE_URL = APP_ENV === 'production' ? 'https://api.pwn2.win' : 'https://nizkctf.herokuapp.com'
-export const CHALLENGES_BASE_URL = APP_ENV === 'production' ? 'https://ctf-br.github.io/ranking/challenges' : 'https://ctf-br.github.io/ranking/challenges'
+export const CHALLENGES_BASE_URL = APP_ENV === 'production' ? 'https://pwn2winctf.github.io/nizkctf-content/challenges' : 'https://ctf-br.github.io/ranking/challenges'
 
 export const START_SUBSCRIPTION_DATE = dayjs('02-05-2021 08:00 -03:00', 'DD-MM-YYYY HH:mm Z').toDate()
 export const START_EVENT_DATE = dayjs('28-05-2021 13:37 -03:00', 'DD-MM-YYYY HH:mm Z').toDate()
