80301db684c874ea73643600dd43f2f6b35edf17
Lorhan Sohaky <lorhanoshaky@gmail.com>
2021-05-28 20:17:47 -0300

    Fix code style

--- lib/solves.ts
+++ lib/solves.ts
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from "../constants"
 import { Score, Solve } from "../interface"
 
-export const SOLVES_URL = new URL('/score',API_BASE_URL).toString()
+export const SOLVES_URL = new URL('/score', API_BASE_URL).toString()
 
 export const getSimpleSolvesList = async () => {
   const sortedSolves: Array<{ team: string, challenge: string, datetime: number }> = await fetchSimpleSolvesList(SOLVES_URL)
